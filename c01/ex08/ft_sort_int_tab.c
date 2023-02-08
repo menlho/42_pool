fc2d5853124afaeb63ea5e7c8258b442a5b72a04
Morgan Masclet <momascle@student.42nice.fr>
2023-02-08 15:21:29 -0500

    quicksort implanted and main updated

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,48 @@
+void	ft_swapel(int *el1, int *el2)
+{
+	int temp;
+	temp = *el1;
+	*el1 = *el2;
+	*el2 = temp;
+}
+
+int	ft_partition(int *tab, int low, int high)
+{
+	int pivot;
+	int i;
+	int j;
+
+	pivot = tab[high];
+	i = low - 1;
+	j = low;
+
+	while (j < high)
+	{
+		if (tab[j] <= pivot)
+		{
+			i++;
+			ft_swapel(&tab[i], &tab[j]);
+		}
+		j++;
+	}
+	ft_swapel(&tab[i + 1], &tab[high]);
+	return(i + 1);
+}
+void	ft_quicksort(int *tab, int low, int high)
+{
+	if (low < high)
+	{
+		int pi;
+		pi = ft_partition(tab, low, high);
+
+		ft_quicksort(tab, low, pi - 1);
+		ft_quicksort(tab, pi + 1, high);
+	}
+}
+void	ft_sort_int_tab(int *tab, int size)
+{
+	if (size >= 0)
+	{
+		ft_quicksort(tab, 0, size - 1);
+	}
+}
